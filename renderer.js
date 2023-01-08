aa41462a24c63ab542cd95a10f72d3d6d410d6d4
singh47 <bravesimran@gmail.com>
2023-01-08 02:40:57 -0600

    Added Vendors and Tree View

--- renderer.js
+++ renderer.js
@@ -1,28 +1,40 @@
 const { exec }  = require('child_process')
 const { stdout, stderr, stdin } = require('process');
 
+
+// execute command and update results
+const executeCommand = (command) => {{
+    exec(command, (error, stdout, stderr) => {
+        if (error){
+            console.error("error executing command")
+            return
+        }
+        document.getElementById("terminal").innerHTML = stdout;
+    })
+}}
+
 // on load function
 async function listUsb() {
-    exec('./lsusb/lsusb -v', (error, stdout, stderr) => {
-    if (error){
-        console.error("error executing command")
-        return
-    }
-    document.getElementById("terminal").innerHTML = stdout;
-    })
+    executeCommand('./lsusb/lsusb -v')
 }
 
 // on refresh
 async function refresh() {
-    exec('./lsusb/lsusb -v', (error, stdout, stderr) => {
-    if (error){
-        console.error("error executing command")
-        return
-    }
-    document.getElementById("terminal").innerHTML = stdout;
-    })
+    executeCommand('./lsusb/lsusb -v')
+}
+
+// vendors
+async function vendors() {
+    executeCommand('./lsusb/lsusb -p')
+}
+
+// tree view
+async function treeView() {
+    executeCommand('./lsusb/lsusb -t')
 }
 
 window.onload = listUsb
 document.getElementById("refresh").addEventListener('click', refresh)
+document.getElementById("vendors").addEventListener('click', vendors)
+document.getElementById("treeView").addEventListener('click', treeView)
                                           
