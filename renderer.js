0159bf641189f08c7e62d133ebc6f4c40a8adf30
singh47 <bravesimran@gmail.com>
2023-01-08 02:18:14 -0600

    initial commit

---
+++ renderer.js
@@ -0,0 +1,28 @@
+const { exec }  = require('child_process')
+const { stdout, stderr, stdin } = require('process');
+
+// on load function
+async function listUsb() {
+    exec('lsusb -v', (error, stdout, stderr) => {
+    if (error){
+        console.error("error executing command")
+        return
+    }
+    document.getElementById("terminal").innerHTML = stdout;
+    })
+}
+
+// on refresh
+async function refresh() {
+    exec('lsusb -v', (error, stdout, stderr) => {
+    if (error){
+        console.error("error executing command")
+        return
+    }
+    document.getElementById("terminal").innerHTML = stdout;
+    })
+}
+
+window.onload = listUsb
+document.getElementById("refresh").addEventListener('click', refresh)
+                                          
